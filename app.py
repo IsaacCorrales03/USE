b16ff30b2d49901ac6cbef443d8783e43536a863
firetpulse <firetpulse@proton.me>
2026-03-13 21:51:42 -0600

    ASD

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, render_template, send_from_directory
+from flask import Flask, jsonify, request, render_template, send_from_directory #APP.PY DEIVID
 from models import db
 from crud import (
     crear_integrante, cambiar_estado_integrante,
